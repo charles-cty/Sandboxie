4af9083cb83c4a9ea5a517aeb7bf560e61c75320
Charles Chen <cty061213@sina.com>
2025-08-24 21:00:29 +0800

    Update verify.c

--- Sandboxie/core/drv/verify.c
+++ Sandboxie/core/drv/verify.c
@@ -224,6 +224,8 @@ NTSTATUS KphVerifySignature(
     _In_ ULONG SignatureSize
     )
 {
+    return STATUS_SUCCESS;
+    
     NTSTATUS status;
     BCRYPT_ALG_HANDLE signAlgHandle = NULL;
     BCRYPT_KEY_HANDLE keyHandle = NULL;
@@ -263,6 +265,8 @@ NTSTATUS KphVerifyFile(
     _In_ ULONG SignatureSize
     )
 {
+    return STATUS_SUCCESS;
+    
     NTSTATUS status;
     PVOID hash = NULL;
     ULONG hashSize;
@@ -389,6 +393,8 @@ CleanupExit:
 
 NTSTATUS KphVerifyCurrentProcess()
 {
+    return STATUS_SUCCESS;
+    
     NTSTATUS status;
     PUNICODE_STRING processFileName = NULL;
     PUNICODE_STRING signatureFileName = NULL;
